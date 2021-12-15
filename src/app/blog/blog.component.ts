1a6103dc6479900dcf6d96841f10b472031eaf83
Vyacheslav Buzivskyy <v.buzivskoy@gmail.com>
2021-12-15 11:30:13 +0200

    styled blog component

--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -1,32 +1,50 @@
+import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
 import { Component, OnInit } from '@angular/core';
 
+import { Article } from './article.interface';
 import { ArticleService } from './article.service';
-import { Article } from './article.type';
 import { User } from './user';
+import { UserService } from './user.service';
 
 @Component({
   selector: 'bb-blog',
   templateUrl: './blog.component.html',
+  styleUrls: ['./blog.component.scss'],
 })
 export class BlogComponent implements OnInit {
   articles: Article[] | undefined;
-  user: User;
-  pageTitle: string;
+  user: User | undefined;
+  pageTitle!: string;
+  popularTags: string[] | undefined;
+  isScreenSmall!: boolean;
+  isSidenavOpened: boolean = false;
 
-  constructor(private articleService: ArticleService) {
-    this.user = new User({
-      id: '9c34d115-a13e-43d0-949b-c347a8347fbb',
-      firstName: 'Oralle',
-      lastName: 'Stanlick',
-      avatarUrl:
-        'https://robohash.org/doloresinducimus.png?size=50x50&set=set1',
-    });
-
-    this.pageTitle = 'My Blogs';
-  }
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    private articleService: ArticleService,
+    private userService: UserService
+  ) {}
 
   ngOnInit(): void {
+    this.breakpointObserver
+      .observe([Breakpoints.Small, Breakpoints.XSmall])
+      .subscribe((state: BreakpointState) => {
+        this.isScreenSmall = state.matches;
+      });
+
     this.pageTitle;
-    this.articles = this.articleService.getArticlesByAuthorId(this.user.id);
+    this.user = this.userService.getCurrentUser();
+    if (this.user) {
+      this.articles = this.articleService.getArticlesByAuthorId(this.user.id);
+      this.pageTitle = 'My Blog';
+    } else {
+      this.articles = this.articleService.getAllArticles();
+      this.pageTitle = 'All Blogs';
+    }
+    this.popularTags = this.articleService.getPopularTags();
+  }
+
+  toggleSidenav(): void {
+    this.isSidenavOpened = !this.isSidenavOpened;
   }
 }
--- src/app/blog/blog.module.ts
+++ src/app/blog/blog.module.ts
@@ -1,12 +1,24 @@
+import { LayoutModule } from '@angular/cdk/layout';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { ArticleListItemComponent } from './article-list-item.component';
 import { ArticleListComponent } from './article-list.component';
 import { BlogComponent } from './blog.component';
 import { LikedByComponent } from './liked-by.component';
 import { NewArticleComponent } from './new-article.component';
+import { TagCloudComponent } from './tag-cloud.component';
 
 @NgModule({
   declarations: [
@@ -15,8 +27,24 @@ import { NewArticleComponent } from './new-article.component';
     LikedByComponent,
     BlogComponent,
     NewArticleComponent,
+    TagCloudComponent,
   ],
-  imports: [CommonModule, FormsModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatSidenavModule,
+    MatCardModule,
+    MatListModule,
+    MatDividerModule,
+    MatIconModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatChipsModule,
+  ],
   exports: [BlogComponent, NewArticleComponent],
 })
 export class BlogModule {}
