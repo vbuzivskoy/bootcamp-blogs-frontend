0ebad93d0ed7b2202e9811a840e754461e700a18
Vyacheslav Buzivskyy <v.buzivskoy@gmail.com>
2021-12-24 15:19:09 +0200

    moved all material components to shared module

--- src/app/blog/blog.module.ts
+++ src/app/blog/blog.module.ts
@@ -1,18 +1,8 @@
-import { LayoutModule } from '@angular/cdk/layout';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { MatChipsModule } from '@angular/material/chips';
-import { MatDividerModule } from '@angular/material/divider';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatIconModule } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
-import { MatListModule } from '@angular/material/list';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatToolbarModule } from '@angular/material/toolbar';
 
+import { MaterialModule } from '../shared';
 import { ArticleListItemComponent } from './article-list-item.component';
 import { ArticleListComponent } from './article-list.component';
 import { BlogComponent } from './blog.component';
@@ -29,22 +19,7 @@ import { TagCloudComponent } from './tag-cloud.component';
     NewArticleComponent,
     TagCloudComponent,
   ],
-  imports: [
-    CommonModule,
-    FormsModule,
-    MatSidenavModule,
-    MatCardModule,
-    MatListModule,
-    MatDividerModule,
-    MatIconModule,
-    LayoutModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatFormFieldModule,
-    ReactiveFormsModule,
-    MatInputModule,
-    MatChipsModule,
-  ],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, MaterialModule],
   exports: [BlogComponent, NewArticleComponent],
 })
 export class BlogModule {}
