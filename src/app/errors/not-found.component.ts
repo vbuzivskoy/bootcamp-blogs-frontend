b1c0835d7b706b289cce22ffacfe3a313ffc3b4b
Vyacheslav Buzivskyy <v.buzivskoy@gmail.com>
2021-12-29 18:05:04 +0200

    add not-found component

---
+++ src/app/errors/not-found.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  template: ` <h1 class="errorMessage">404'd</h1> `,
+  styleUrls: ['./not-found.component.scss'],
+})
+export class NotFoundComponent {
+  constructor() {}
+}
