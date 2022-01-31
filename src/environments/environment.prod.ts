9af31ab3ab42552a59c204362c1c4f1ea1a07509
Vyacheslav Buzivskyy <v.buzivskoy@gmail.com>
2022-01-31 22:01:27 +0200

    fix api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl:
-    'http://bootcampblogsapiserver-env.eba-6239c5pq.eu-central-1.elasticbeanstalk.com',
+    'http://bootcampblogsapiserver-env.eba-6239c5pq.eu-central-1.elasticbeanstalk.com/api/v1',
 };
